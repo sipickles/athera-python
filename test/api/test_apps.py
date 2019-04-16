d73c08e242efb136231d756fbad3094a5924b1ba
Simon Pickles <simon.pickles@foundry.com>
2019-04-16 21:09:46 +0100

    Using nose2 params to generate tests from a table of parameters and outcomes

--- test/api/test_apps.py
+++ test/api/test_apps.py
@@ -2,71 +2,71 @@ from settings import environment
 from athera.api import apps
 
 import unittest
+from nose2.tools import params
 import uuid
 from requests import codes
 import os
 
 class AppsTest(unittest.TestCase):
+
+    GET_APP_FAMILIES_PARAMETERS = (
+        # (group id, expected_status_code, error_message)
+        (environment.ATHERA_API_TEST_GROUP_ID,       codes.ok,                      "Positive test failed to get app families with valid group id"),
+        (str(uuid.uuid4()),                          codes.forbidden,               "Negative test should fail with a random group id"),
+        (environment.ATHERA_API_TEST_OTHER_GROUP_ID, codes.forbidden,               "Negative test should fail with an inaccessible group id"),
+        ("marginwalker",                             codes.internal_server_error,   "Negative test should fail with a junk group id"),
+    )
+
+    GET_APP_PARAMETERS = (
+        # (group id, app_id, expected_status_code, error_message)
+        (environment.ATHERA_API_TEST_GROUP_ID,          environment.ATHERA_API_TEST_INTERACTIVE_APP_ID, codes.ok,                      "Positive test failed to get app with valid group id and app id"),
+        (environment.ATHERA_API_TEST_GROUP_ID,          str(uuid.uuid4()),                              codes.not_found,               "Positive test should 404 with valid group id but invalid app id"),
+        (str(uuid.uuid4()),                             environment.ATHERA_API_TEST_INTERACTIVE_APP_ID, codes.forbidden,               "Negative test should fail with an invalid id and valid app id"),
+        (str(uuid.uuid4()),                             str(uuid.uuid4()),                              codes.forbidden,               "Negative test should fail with an invalid group id and invalid app id"),
+        (environment.ATHERA_API_TEST_OTHER_GROUP_ID,    environment.ATHERA_API_TEST_INTERACTIVE_APP_ID, codes.forbidden,               "Negative test should fail with an inaccessible group id and valid app id"),
+        (environment.ATHERA_API_TEST_OTHER_GROUP_ID,    str(uuid.uuid4()),                              codes.forbidden,               "Negative test should fail with an inaccessible group id and invalid app id"),
+        ("inonthekilltaker",                            environment.ATHERA_API_TEST_INTERACTIVE_APP_ID, codes.internal_server_error,   "Negative test should fail with a junk group id and valid app id"),
+        ("reclamation",                                 str(uuid.uuid4()),                              codes.internal_server_error,   "Negative test should fail with a junk group id and invalid app id"),
+        ("reclamation",                                 "styrofoam",                                    codes.internal_server_error,   "Negative test should fail with a junk group id and junk app id"),
+    )
+
+
     @classmethod
     def setUpClass(cls):
         cls.token = os.getenv("ATHERA_API_TEST_TOKEN")
         if not cls.token:
             raise ValueError("ATHERA_API_TEST_TOKEN environment variable must be set")
 
-    def test_get_app_families(self):
-        """ Positive test """
-        response = apps.get_app_families(
-            environment.ATHERA_API_TEST_BASE_URL,
-            environment.ATHERA_API_TEST_GROUP_ID,
-            self.token,
-        )
-        self.assertEqual(response.status_code, codes.ok)
-        data = response.json()
-        families = data['families'] 
-        self.assertNotEqual(len(families), 0)
-        first_family = families[0]
-        self.assertNotEqual(len(first_family), 0)
-        self.assertIn("id", first_family)
 
-    def test_get_app_families_bad_id(self):
-        """ Negative test using a random group id """
+    @params(*GET_APP_FAMILIES_PARAMETERS)
+    def test_get_app_families(self, group_id, expected_status_code, error_message):
+        """ Parameterized Test """
         response = apps.get_app_families(
             environment.ATHERA_API_TEST_BASE_URL,
-            str(uuid.uuid4()), 
+            group_id,
             self.token,
         )
-        self.assertEqual(response.status_code, codes.forbidden)
-        
-    def test_get_app_families_wrong_id(self):
-        """ Negative test to check we handle an existing but inaccessible group in header """
-        response = apps.get_app_families(
-            environment.ATHERA_API_TEST_BASE_URL,
-            environment.ATHERA_API_TEST_OTHER_GROUP_ID,
-            self.token,
-        )
-        self.assertEqual(response.status_code, codes.forbidden, "Expected error code FORBIDDEN (403) when providing a wrong family id, Got {}".format(response.status_code))
-        
+        self.assertEqual(response.status_code, expected_status_code, error_message)
 
-    def test_get_app(self):
+        if response.status_code == 200:
+            data = response.json()
+            families = data['families'] 
+            self.assertNotEqual(len(families), 0)
+            first_family = families[0]
+            self.assertNotEqual(len(first_family), 0)
+            self.assertIn("id", first_family)
+
+    @params(*GET_APP_PARAMETERS)
+    def test_get_app(self, group_id, app_id, expected_status_code, error_message):
         """ Positive test """
         response = apps.get_app(
             environment.ATHERA_API_TEST_BASE_URL,
-            environment.ATHERA_API_TEST_GROUP_ID,
+            group_id,
             self.token,
-            environment.ATHERA_API_TEST_INTERACTIVE_APP_ID,
+            app_id,
         )
-        self.assertEqual(response.status_code, codes.ok)
-        app = response.json()
-        self.assertIn("id", app)
-        self.assertEqual(environment.ATHERA_API_TEST_INTERACTIVE_APP_ID, app['id'])
-
-    def test_get_app_bad_id(self):
-        """ Negative test using a random app """
-        response = apps.get_app(
-            environment.ATHERA_API_TEST_BASE_URL,
-            environment.ATHERA_API_TEST_GROUP_ID,
-            self.token,
-            str(uuid.uuid4()),
-        )
-        self.assertEqual(response.status_code, codes.not_found)
-        
+        self.assertEqual(response.status_code, expected_status_code)
+        if response.status_code == 200:
+            app = response.json()
+            self.assertIn("id", app)
+            self.assertEqual(environment.ATHERA_API_TEST_INTERACTIVE_APP_ID, app['id'])
